e8ac6f28efc64956482e63196dfee2fcbd468d60
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-10 22:41:55 +0300

    refactor compile so we compile once and use everywhere

--- etherium/compile.js
+++ etherium/compile.js
@@ -1,58 +1,56 @@
-// // compile code will go here
-// const path = require('path');
-// const fs = require('fs');
-// const solc = require('solc');
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
 
-// const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-// const src = fs.readFileSync(inboxPath, 'utf8');
+// 1. Delete build directory entirely
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
 
-// const compileResult = solc.compile(src, 1);
+// 2. Read in Campaign.sol
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+try {
+    const src = fs.readFileSync(campaignPath, 'utf-8');
 
-// console.log(src)
-
-// console.log("compiled: ")
-// console.log(compileResult)
-// console.log("compiled: ")
-
-const path = require('path')
-const fs = require('fs')
-const solc = require('solc')
-
-// Path to your Solidity contract
-const filePath = path.resolve(__dirname, 'contracts', 'Lottery.sol')
-const source = fs.readFileSync(filePath, 'utf8')
-
-// Set up the input structure for the Solidity compiler
-const input = {
-    language: 'Solidity',
-    sources: {
-        'Campaign.sol': {
-            content: source,
-        },
-    },
-    settings: {
-        outputSelection: {
-            '*': {
-                '*': ['abi', 'evm.bytecode'],
+    // 3. Prepare input for the Solidity compiler
+    const input = {
+        language: 'Solidity',
+        sources: {
+            'Campaign.sol': {
+                content: src,
             },
         },
-    },
-}
+        settings: {
+            outputSelection: {
+                '*': {
+                    '*': ['abi', 'evm.bytecode.object'],
+                },
+            },
+        },
+    };
 
-// Callback function for reading files (not used in this case, but required)
-const findImports = (importPath) => {
-    try {
-        const filePath = path.resolve(__dirname, 'contracts', importPath)
-        const content = fs.readFileSync(filePath, 'utf8')
-        return { contents: content }
-    } catch (error) {
-        return { error: 'File not found' }
+    // 4. Compile the contract
+    const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+    // Check for compilation errors
+    if (output.errors) {
+        output.errors.forEach((err) => {
+            console.error(err.formattedMessage);
+        });
+        throw new Error('Compilation failed');
     }
+
+    // Make sure the build directory exists
+    fs.ensureDirSync(buildPath);
+
+    // 5. Write the output to the build directory
+    for (const contractName in output.contracts['Campaign.sol']) {
+        const contract = output.contracts['Campaign.sol'][contractName];
+        fs.outputJsonSync(
+            path.resolve(buildPath, `${contractName}.json`),
+            contract
+        );
+    }
+
+} catch (error) {
+    console.log('Error when dealing with the file: ', error);
 }
-
-// Compile the contract
-const output = JSON.parse(solc.compile(JSON.stringify(input), { import: findImports }))
-console.log(output.contracts['Campaign.sol'].Campaign)
-
-// Export the ABI and bytecode
-module.exports = output.contracts['Campaign.sol'].Campaign
