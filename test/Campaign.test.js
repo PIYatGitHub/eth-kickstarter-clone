3cd3660b343d131c3da9ca7fc94945584700b85a
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-11 21:09:49 +0300

    one more test

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -1,52 +1,78 @@
-const assert = require("assert");
-const ganache = require("ganache-cli");
+const assert = require("assert")
+const ganache = require("ganache-cli")
 const { Web3 } = require("web3") // pay attention to capital and lowercase letters!
 const web3 = new Web3(ganache.provider())
 
-const compiledFactory = require("../etherium/build/CampaignFactory.json");
-const compiledCampaign = require("../etherium/build/Campaign.json");
+const compiledFactory = require("../etherium/build/CampaignFactory.json")
+const compiledCampaign = require("../etherium/build/Campaign.json")
 
 
 
-let accounts; // a list of all accounts we had
-let factory; // a ref to the deployed factory we have
-let campaignAddress; // where is the new campaign deployed?
-let campaign; // create an instance we can interact with
-let manager; // who deployed the contract
+let accounts // a list of all accounts we had
+let factory // a ref to the deployed factory we have
+let campaignAddress // where is the new campaign deployed?
+let campaign // create an instance we can interact with
+let manager // who deployed the contract
 
 beforeEach(async () => {
-    accounts = await web3.eth.getAccounts();
-    manager = accounts[0];
+    accounts = await web3.eth.getAccounts()
+    manager = accounts[0]
     console.log(manager)
 
     const commonSendParams = {
         from: manager,
         gas: "3000000", // Increased gas limit
         gasPrice: "200000"
-    };
+    }
 
     factory = await new web3.eth.Contract(compiledFactory.abi)
         .deploy({ data: compiledFactory.evm.bytecode.object })
-        .send(commonSendParams);
+        .send(commonSendParams)
 
-    console.log("TESTS >> ", "contract deployed at: ", factory.options.address);
+    console.log("TESTS >> ", "contract deployed at: ", factory.options.address)
 
-    await factory.methods.createCampaign('10000').send(commonSendParams);
+    await factory.methods.createCampaign('10000').send(commonSendParams)
 
-    const deployedCampaigns = await factory.methods.getDeployedCapmpaigns().call();
-    campaignAddress = deployedCampaigns[0];
+    const deployedCampaigns = await factory.methods.getDeployedCapmpaigns().call()
+    campaignAddress = deployedCampaigns[0]
 
     campaign = await new web3.eth.Contract(
         compiledCampaign.abi,
         campaignAddress
-    );
-});
+    )
+})
 
 describe('Campaigns', () => {
     it('can deploy CampaignFactory', () => {
-        assert.ok(factory.options.address);
-    });
+        assert.ok(factory.options.address)
+    })
+
     it('can deploy Campaign', () => {
-        assert.ok(campaign.options.address);
-    });
-});
+        assert.ok(campaign.options.address)
+    })
+
+    it('marks sender as manager of the Campaign', async () => {
+        const mgrComp = await campaign.methods.manager().call()
+        assert.equal(mgrComp, manager)
+    })
+
+    it('allows ppl to contribute money and marks them as approvers', async () => {
+        const contributor = accounts[1]
+        await campaign.methods.contribute().send({
+            value: '120000',
+            from: contributor,
+            gas: "3000000", // Increased gas limit
+            gasPrice: "200000"
+        })
+
+        console.log("here?")
+
+        const count = await campaign.methods.approversCount().call()
+        assert.equal(count, 1)
+
+        const isApprover = await campaign.methods.approvers(contributor).call()
+        assert.equal(isApprover, true)
+    })
+
+    
+})
