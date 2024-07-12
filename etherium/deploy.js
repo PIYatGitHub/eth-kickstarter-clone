5aaf841c446b9fa28dc1679fecd8c05cb3dc6c72
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-12 22:26:28 +0300

    change deploy - probably not logged in to metamsk

--- etherium/deploy.js
+++ etherium/deploy.js
@@ -15,18 +15,26 @@ const provider = new HDWalletProvider(
 const web3 = new Web3(provider)
 
 async function deploy() {
-    const accounts = await web3.eth.getAccounts()
-    const selectedAccount = accounts.at(0)
+    try {
+        const accounts = await web3.eth.getAccounts()
+        const selectedAccount = accounts.at(0)
 
-    console.log("Attempting to deploy with account: ", selectedAccount)
+        console.log("Attempting to deploy with account: ", selectedAccount)
 
-    //deploy the contract to one of them 
-    const resultInstance = await new web3.eth.Contract(compiledFactory.abi)
-        .deploy({ data: compiledFactory.evm.bytecode.object })
-        .send({ from: selectedAccount, gas: '1000000' })
+        const balance = await web3.eth.getBalance(selectedAccount)
+        console.log("Account balance: ", balance)
 
-    console.log("This contract just got deployed to this address!", JSON.stringify(compiledFactory.abi))
-    console.log("This contract just got deployed to this address!", resultInstance.options.address)
+        //deploy the contract to one of them 
+        const resultInstance = await new web3.eth.Contract(compiledFactory.abi)
+            .deploy({ data: compiledFactory.evm.bytecode.object })
+            .send({ from: selectedAccount, gas: '5000000' })
+
+        console.log("This contract just got deployed to this address!", JSON.stringify(compiledFactory.abi))
+        console.log("This contract just got deployed to this address!", resultInstance.options.address)
+    } catch (error) {
+        console.log(error)
+    }
+
 }
 
 // we made this so we can use the async/await! 
