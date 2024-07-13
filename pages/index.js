15af847c3861f30a74feba23990d2578ec3be096
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-13 09:12:49 +0300

    play around with content on main page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,45 @@
 import React, { Component } from "react"
 import factory from "../etherium/campaign-factory"
+import { Button, Card } from "semantic-ui-react"
 
 class CampaignIndex extends Component {
-    async componentDidMount() {
+    static async getInitialProps() {
         const deployedCampaigns = await factory.methods.getDeployedCapmpaigns().call()
         console.log("DC = ", deployedCampaigns)
+
+        return {
+            campaigns: deployedCampaigns.length > 0 ? deployedCampaigns : ["0xeE1b01000F075f8677eBBF1cD52903735e11111"]
+        }
+    }
+
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>Details</a>,
+                fluid: true
+            }
+        })
+
+        console.log('items....', items)
+
+        return <Card.Group items={items} />
     }
 
     render() {
-        <p>TTTTT</p>
+        return <div>
+            <link
+                async
+                rel="stylesheet"
+                href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+            />
+            {this.renderCampaigns()}
+            <Button
+                content="Add new campaign"
+                icon="add circle"
+                primary
+            />
+        </div>
     }
 }
 
