3e6c8f67bc0d6443db7e5e84b02bc284a5babc49
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-13 15:38:17 +0300

    error handling in form

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,16 +1,40 @@
 import React, { Component } from "react"
 import Layout from "../../components/layout"
-import { Button, Form, Input } from "semantic-ui-react"
+import { Button, Form, Input, Message } from "semantic-ui-react"
+import campaignFactory from "../../etherium/campaign-factory"
+import web3 from "../../etherium/web3"
 
 class CampaignNew extends Component {
     state = {
-        minimumContribution: '1000000'
+        minimumContribution: '1000000',
+        requestError: ""
     }
+
+    onSubmit = async (e) => {
+        e.preventDefault()
+        console.log("Submitting that form")
+        console.log("Submitting with this amount of Wei", this.state.minimumContribution)
+        try {
+            const accounts = await web3.eth.getAccounts()
+            await campaignFactory.methods.createCampaign(
+                this.state.minimumContribution
+            ).send({
+                from: accounts[0]
+            })
+        } catch (error) {
+            this.setState({ requestError: "Ooops something went worng. Please try again later!" })
+            setTimeout(() => {
+                this.setState({ requestError: "" })
+            }, 3500)
+        }
+
+    }
+
     render() {
         return (<Layout>
             <h2>Let's go make something great!</h2>
 
-            <Form>
+            <Form onSubmit={this.onSubmit}>
                 <Form.Field>
                     <label>Minimum contribution (in Wei) 10 000 000 000 000 000 wei = $3.15</label>
                     <Input
@@ -18,18 +42,21 @@ class CampaignNew extends Component {
                         labelPosition="right"
                         icon="money"
                         iconPosition="left"
-                        type="number"
-                        min={1000000}
-                        max={1000000000000000}
-                        step={100000}
-                        defaultValue={1000000}
-                        value={Number(this.state.minimumContribution)}
+                        //type="number"
+                        // min={1000000}
+                        // max={1000000000000000}
+                        // step={100000}
+                        // defaultValue={1000000}
+                        value={this.state.minimumContribution}
                         onChange={(e) => this.setState({ minimumContribution: e.target.value })}
                     />
 
                 </Form.Field>
 
-                <Button primary>Create!</Button>
+                <Button primary disabled={this.state.requestError !== ""}>Create!</Button>
+                {this.state.requestError !== "" ? (
+                    <Message negative header="Error!" content={this.state.requestError} />
+                ) : null}
             </Form>
         </Layout>)
     }
