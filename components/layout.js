68c03c2bb79f58921a99ae32b1da2dae4f972bfe
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-13 12:42:40 +0300

    move styles to head of page

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,17 @@
 import React from "react"
 import Header from "./header"
 import { Container } from "semantic-ui-react"
+import Head from "next/head"
 
 export default props => {
     return <Container>
-        <link
-            async
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
-        />
+        <Head>
+            <link
+                async
+                rel="stylesheet"
+                href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+            />
+        </Head>
         <Header />
         {props.children}
     </Container>
