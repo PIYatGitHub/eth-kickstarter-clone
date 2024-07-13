ed5b45d618e635e2dc149e869ab37d24cac31af2
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-13 16:12:09 +0300

    add some more comments in routes.

--- routes.js
+++ routes.js
@@ -1,3 +1,8 @@
+// because next does not handle routes with dynamic content we need to use this kind of approach.
+// hence this file will hold all the dynamic routes we have. 
+// Important! The default route handling is still at play too. So all routes not handled here will be handled by the default router. -_- :0 
+
+
 const routes = require("next-routes")() // absolutely critical to use the second "()" since this returns a function! 
 
 
