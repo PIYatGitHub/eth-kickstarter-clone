6ebfee535df04bf5f25b86f951945e5734fe1445
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-13 16:06:01 +0300

    finish config

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const next = require('next')
 const routes = require('./routes')
 const app = next({ dev: process.env.NODE_ENV !== 'production' })
 const handler = routes.getRequestHandler(app)
-
-
 const { createServer } = require('http')
+
 app.prepare().then(() => {
-    createServer(handler).listen(3333)
+    createServer(handler).listen(3333, (err) => {
+        if (err) throw err
+        console.log("App is listening on port 3333!")
+    })
 })
