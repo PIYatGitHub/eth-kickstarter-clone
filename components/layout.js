bd11a1c74c09d41c5b0273564978c4807ef95e8b
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-13 12:40:54 +0300

    work on layout

--- components/layout.js
+++ components/layout.js
@@ -4,6 +4,11 @@ import { Container } from "semantic-ui-react"
 
 export default props => {
     return <Container>
+        <link
+            async
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+        />
         <Header />
         {props.children}
     </Container>
