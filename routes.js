00cd1319e4cddbdd09c2bc8b0b3f52032f0eab7d
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-14 16:30:36 +0300

    extend routes

--- routes.js
+++ routes.js
@@ -8,6 +8,7 @@ const routes = require("next-routes")() // absolutely critical to use the second
 routes.add("/campaigns/new", "/campaigns/new")
 routes.add("/campaigns/:address", "/campaigns/show")
 routes.add("/campaigns/:address/requests", "/campaigns/requests/index")
+routes.add("/campaigns/:address/requests/new", "/campaigns/requests/new")
 
 
 module.exports = routes
