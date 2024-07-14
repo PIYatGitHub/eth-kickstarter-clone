2afd2e29e57e32118e026bc85d2ec8de290304d8
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-14 15:26:50 +0300

    first card

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import Layout from "../../components/layout"
-import { Button, Form, Input, Message } from "semantic-ui-react"
+import { Button, Card, Form, Input, Message } from "semantic-ui-react"
 import campaignFactory from "../../etherium/campaign-factory"
 import web3 from "../../etherium/web3"
 import { Router } from "../../routes"
@@ -16,12 +16,28 @@ class CampaignShow extends Component {
         console.log('data', data)
 
         return {
-            minimumContribution: data[0],
-            campaignBalance: data[1],
-            requestsCount: data[2],
-            approversCount: data[3],
-            manager: data[4],
+            minimumContribution: data[0].toString(),
+            balance: data[1].toString(),
+            requestsCount: data[2].toString(),
+            approversCount: data[3].toString(),
+            manager: data[4].toString(),
         }
+    }
+
+
+    renderCards() {
+        const { minimumContribution, balance, requestsCount, approversCount, manager } = this.props
+        console.log('what is in props? ', this.props)
+        const items = [
+            {
+                header: manager,
+                meta: "Each campaign has a manager. This is his address.",
+                description: "The manager is the one who made this campaign. He can make requests to withdraw money.",
+                style: { overflowWrap: 'break-word' }
+            }
+        ]
+
+        return <Card.Group items={items} />
     }
 
 
@@ -29,6 +45,7 @@ class CampaignShow extends Component {
         return (<Layout>
 
             <h2>Campaign details!</h2>
+            {this.renderCards()}
 
         </Layout>)
     }
