ba88b2d1a18e9b41df68c946d60506da6f6ffd4d
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-14 15:49:02 +0300

    work on the form

--- components/contribute.js
+++ components/contribute.js
@@ -1,8 +1,41 @@
 import React, { Component } from "react"
 import { Button, Form, Input } from "semantic-ui-react"
+import getCampaignById from "../etherium/campaign"
 
 
 class ContributeForm extends Component {
+    state = {
+        pledge: '',
+        requestError: '',
+        requestLoading: false
+    }
+
+
+    onSubmit = async () => {
+        e.preventDefault()
+        this.setState({ requestLoading: true })
+        try {
+            const accounts = await web3.eth.getAccounts()
+            const campaign = getCampaignById("")
+            await campaign.methods.contribute(this.state.pledge).send({
+                from: accounts[0]
+            })
+
+            Router.pushRoute('/')
+
+        } catch (error) {
+            this.setState({ requestError: "Ooops something went worng. Please try again later!" })
+            setTimeout(() => {
+                this.setState({ requestError: "" })
+            }, 2500)
+        } finally {
+            this.setState({ requestLoading: false })
+        }
+
+
+    }
+
+
     render() {
         return (
             <Form>
@@ -11,9 +44,10 @@ class ContributeForm extends Component {
                     <Input
                         label="ETH"
                         labelPosition="right"
+                        onChange={(e) => this.setState({ pledge: e.target.value })}
                     />
                 </Form.Field>
-                <Button primary>
+                <Button primary loading={this.state.requestLoading} disabled={this.state.requestError !== ""}>
                     Contribute!
                 </Button>
             </Form>
