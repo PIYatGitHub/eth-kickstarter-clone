452e2b5d287d3a4abfa4f03d382c32317c1d751b
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-14 16:30:58 +0300

    reroute to creating request

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react"
 import Layout from "../../../components/layout"
+import { Button } from "semantic-ui-react"
+import { Link } from "../../../routes"
 
 class RequestsIndex extends Component {
+    static async getInitalProps(props) {
+        const { address } = props.query
+
+        return { address }
+    }
 
     render() {
         return (<Layout>
-            <h3>Request List!</h3>
+            <h3>Requests</h3>
+            <Link route={`/campaigns/${this.props.address}/requests/new`} legacyBehavior>
+                <a>
+                    <Button primary>
+                        Add request
+                    </Button>
+                </a>
+            </Link>
+
         </Layout>)
     }
 }
