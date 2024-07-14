1dc4a5afd8cbcf4d5bb71ea44a0322e66d53084d
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-14 17:28:07 +0300

    fixing stuff -- wip

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -4,18 +4,18 @@ import { Button } from "semantic-ui-react"
 import { Link } from "../../../routes"
 
 class RequestsIndex extends Component {
-    static async getInitalProps(props) {
+    static async getInitialProps(props) {
         const { address } = props.query
-
         return { address }
     }
 
     render() {
         return (<Layout>
             <h3>Requests</h3>
+            <h3>{Object.values(this.props)}</h3>
             <Link route={`/campaigns/${this.props.address}/requests/new`} legacyBehavior>
                 <a>
-                    <Button primary>
+                    <Button primary disabled={!this.props.address}>
                         Add request
                     </Button>
                 </a>
--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,19 +1,83 @@
 import React, { Component } from "react"
 import Layout from "../../../components/layout"
-import { Button } from "semantic-ui-react"
-import { Link } from "../../../routes"
+import { Button, Form, Input, Message } from "semantic-ui-react"
+import getCampaignById from "../../../etherium/campaign"
+import web3 from "../../../etherium/web3"
 
 class RequestForm extends Component {
-    static async getInitalProps(props) {
-        const { address } = props.query
+    state = {
+        description: '',
+        value: '',
+        recepient: '',
+        requestError: '',
+        requestLoading: false
+    }
 
+    static async getInitialProps(props) {
+        const { address } = props.query
         return { address }
+    }
+
+    onSubmit = async (e) => {
+        e.preventDefault()
+        this.setState({ requestLoading: true })
+        try {
+            const accounts = await web3.eth.getAccounts()
+            console.log("PROPS???", this.props)
+            const campaign = getCampaignById(this.props.address)
+            const { description, value, recepient } = this.state
+
+            await campaign.methods.createRequest(
+                description,
+                web3.utils.toWei(value),
+                recepient
+            ).send({
+                from: accounts[0]
+            })
+        } catch (error) {
+            this.setState({ requestError: "Ooops something went worng. Please try again later!" })
+            setTimeout(() => {
+                this.setState({ requestError: "" })
+            }, 2500)
+        } finally {
+            this.setState({ requestLoading: false })
+            //Router.replaceRoute(`/campaigns/${this.props.address}`)
+        }
     }
 
     render() {
         return (
             <Layout>
-                <h3>Add a request</h3>
+                <h3>Create a request</h3>
+                <Form onSubmit={this.onSubmit}>
+                    <Form.Field>
+                        <label>Description</label>
+                        <Input
+                            value={this.state.description}
+                            onChange={(e) => { this.setState({ description: e.target.value }) }}
+                        />
+                    </Form.Field>
+
+                    <Form.Field>
+                        <label>Value in ETH</label>
+                        <Input
+                            value={this.state.value}
+                            onChange={(e) => { this.setState({ value: e.target.value }) }}
+                        />
+                    </Form.Field>
+
+                    <Form.Field>
+                        <label>Recepient</label>
+                        <Input
+                            value={this.state.recepient}
+                            onChange={(e) => { this.setState({ recepient: e.target.value }) }}
+                        />
+                    </Form.Field>
+
+                    <Button primary loading={this.state.requestLoading} disabled={this.state.requestError !== ""}>
+                        Create!
+                    </Button>
+                </Form>
             </Layout>
         )
     }
