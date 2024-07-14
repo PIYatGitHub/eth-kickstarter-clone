2c441201f6b3b465051496d1bb6ae0ea5c39fafc
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-14 23:58:07 +0300

    start work on a table of data

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,18 +1,30 @@
 import React, { Component } from "react"
 import Layout from "../../../components/layout"
-import { Button } from "semantic-ui-react"
+import { Button, Table } from "semantic-ui-react"
 import { Link } from "../../../routes"
+import getCampaignById from "../../../etherium/campaign"
 
 class RequestsIndex extends Component {
     static async getInitialProps(props) {
         const { address } = props.query
-        return { address }
+        const campaign = getCampaignById(address)
+        const requestsCount = await campaign.methods.getRequestsCount().call()
+        let requests = await Promise.all(
+            Array(requestsCount).fill().map((element, idx) => {
+                return campaign.methods.requests(idx).call()
+            })
+        )
+
+        console.log("resolved requests for len =", requestsCount, requests)
+
+        return { address, requestsCount, requests }
     }
 
     render() {
+        const { Header, Row, HeaderCell, Body } = Table
         return (<Layout>
-            <h3>Requests</h3>
-            <h3>{Object.values(this.props)}</h3>
+
+            <h3>Pending Requests</h3>
             <Link route={`/campaigns/${this.props.address}/requests/new`} legacyBehavior>
                 <a>
                     <Button primary disabled={!this.props.address}>
@@ -21,6 +33,19 @@ class RequestsIndex extends Component {
                 </a>
             </Link>
 
+            <Table>
+                <Header>
+                    <Row>
+                        <HeaderCell>Id</HeaderCell>
+                        <HeaderCell>Description</HeaderCell>
+                        <HeaderCell>Amount</HeaderCell>
+                        <HeaderCell>Recepient</HeaderCell>
+                        <HeaderCell>ApprovalCount</HeaderCell>
+                        <HeaderCell>Approve</HeaderCell>
+                        <HeaderCell>Finalize</HeaderCell>
+                    </Row>
+                </Header>
+            </Table>
         </Layout>)
     }
 }
