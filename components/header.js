dc6de501847f79a1fe7b2b10c73d88d7e84fbf04
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-14 12:15:33 +0300

    make nav work

--- components/header.js
+++ components/header.js
@@ -1,13 +1,13 @@
 import React from "react"
 import { Menu } from "semantic-ui-react"
-
+import { Link } from "../routes"
 
 export default () => {
     return <Menu style={{ marginTop: '1rem' }}>
-        <Menu.Item>CrowdCoin</Menu.Item>
+        <Link route="/" legacyBehavior><a className="item">CrowdCoin</a></Link>
         <Menu.Menu position="right">
-            <Menu.Item>Campaigns</Menu.Item>
-            <Menu.Item>+</Menu.Item>
+            <Link route="/" legacyBehavior><a className="item">Campaigns</a></Link>
+            <Link route="/campaigns/new" legacyBehavior><a className="item">+</a></Link>
         </Menu.Menu>
 
     </Menu>
