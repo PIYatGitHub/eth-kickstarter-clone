34f492a36fa8712987b86769a022e958a6a07fea
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-14 12:25:04 +0300

    simple component

---
+++ pages/campaigns/show.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react"
+import Layout from "../../components/layout"
+import { Button, Form, Input, Message } from "semantic-ui-react"
+import campaignFactory from "../../etherium/campaign-factory"
+import web3 from "../../etherium/web3"
+import { Router } from "../../routes"
+
+class CampaignShow extends Component {
+    state = {}
+
+
+
+    render() {
+        return (<Layout>
+
+            <h2>Campaign details!</h2>
+
+        </Layout>)
+    }
+}
+
+export default CampaignShow
