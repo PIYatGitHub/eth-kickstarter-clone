4db844551b0b3e74b2ca74dedeea6bfb3e08fa68
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2024-07-15 22:51:07 +0300

    two functional buttons

--- components/request-row.js
+++ components/request-row.js
@@ -1,8 +1,51 @@
 import React, { Component } from "react"
-import { Table } from "semantic-ui-react"
+import { Button, Table } from "semantic-ui-react"
 import web3 from "../etherium/web3"
+import getCampaignById from "../etherium/campaign"
 
 class RequestRow extends Component {
+    state = {
+        requestError: '',
+        requestLoading: false
+    }
+
+    onApprove = async () => {
+        try {
+            const accounts = await web3.eth.getAccounts()
+            const campaign = getCampaignById(this.props.address)
+
+            await campaign.methods.approveRequest(this.props.id).send({
+                from: accounts[0]
+            })
+        } catch (error) {
+            this.setState({ requestError: "Ooops something went worng. Please try again later!" + error.message })
+            setTimeout(() => {
+                this.setState({ requestError: "" })
+            }, 2500)
+        } finally {
+            this.setState({ requestLoading: false })
+        }
+    }
+
+
+    onFinalize = async () => {
+        try {
+            const accounts = await web3.eth.getAccounts()
+            const campaign = getCampaignById(this.props.address)
+
+            await campaign.methods.finalizeRequest(this.props.id).send({
+                from: accounts[0]
+            })
+        } catch (error) {
+            this.setState({ requestError: "Ooops something went worng. Please try again later!" + error.message })
+            setTimeout(() => {
+                this.setState({ requestError: "" })
+            }, 2500)
+        } finally {
+            this.setState({ requestLoading: false })
+        }
+    }
+
     render() {
         const { Row, Cell } = Table
         const { id, request, approvers } = this.props
@@ -14,8 +57,16 @@ class RequestRow extends Component {
                 <Cell>{web3.utils.fromWei(value, 'ether')}</Cell>
                 <Cell>{recepient}</Cell>
                 <Cell>{approvalsCount} / {approvers}</Cell>
-                <Cell>Approve</Cell>
-                <Cell>Finalize</Cell>
+                <Cell>
+                    <Button color="green" basic onClick={this.onApprove} loading={this.state.requestLoading}>
+                        Approve
+                    </Button>
+                </Cell>
+                <Cell>
+                    <Button color="red" basic onClick={this.onFinalize} loading={this.state.requestLoading}>
+                        Finalize
+                    </Button>
+                </Cell>
             </Row>
         )
     }
